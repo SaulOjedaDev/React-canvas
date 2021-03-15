decab95e04fede779e1b069278ff65e3543042e4
SaulOjedaDev <isacc.ojeda.dev@gmail.com>
2021-03-15 07:31:55 -0600

    added scrollTo top on load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import CanvasScreen from "./screens/canvasScreen/CanvasScreen";
 
 function App() {
+  useEffect(() => {
+    window?.scrollTo(0, 1);
+  }, []);
+
   return (
     <Fragment>
       <CanvasScreen />
